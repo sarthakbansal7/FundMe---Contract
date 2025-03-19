82bc9497e4a090b5ab2c6bb25ec32c695314a215
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-19 18:27:47 +0530

    Creating PriceConverter Library

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
+
+// Why is this a library and not abstract?
+// Why not an interface?
+library PriceConverter {
+    
+}
