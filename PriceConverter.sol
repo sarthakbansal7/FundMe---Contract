17dc9e04f306a6827f8a3878cc08788c5bd633fa
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-19 18:29:23 +0530

    Add getPrice() Function

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -3,8 +3,15 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
 
-// Why is this a library and not abstract?
-// Why not an interface?
 library PriceConverter {
-    
+  
+    function getPrice() internal view returns (uint256) {
+        // Sepolia ETH / USD Address
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
+    } 
 }
