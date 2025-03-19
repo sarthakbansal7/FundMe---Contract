f554ad8c678c4d9a85eaacad16df4031fbf3a47f
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-19 18:37:37 +0530

    Add Fund Function

--- FundMe.sol
+++ FundMe.sol
@@ -20,5 +20,12 @@ using PriceConverter for uint256;
     constructor() {
         i_owner = msg.sender;
     }
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
     
 }
