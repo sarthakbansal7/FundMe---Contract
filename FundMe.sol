25032ecf94426123f02252cd9fdf5c37506047a2
Sarthak Bansal <sarthakbansalog@gmail.com>
2025-03-19 18:36:53 +0530

    Update FundMe.sol

--- FundMe.sol
+++ FundMe.sol
@@ -2,12 +2,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-// Note: The AggregatorV3Interface might be at a different location than what was in the video!
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
 
 contract FundMe {
+using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    
+    address public i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
     
 }
